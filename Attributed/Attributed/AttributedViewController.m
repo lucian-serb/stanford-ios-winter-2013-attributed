8545b982ea3303a33d012649715ee3d1f7333af9
Andrei-Lucian Șerb <lucian.serb@icloud.com>
2013-09-05 13:59:57 +0300

    Update viewDidLoad method to set the selected word label text when the view has done loading

--- Attributed/Attributed/AttributedViewController.m
+++ Attributed/Attributed/AttributedViewController.m
@@ -43,7 +43,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self updateSelectedWord];
 }
 
 - (void)didReceiveMemoryWarning
